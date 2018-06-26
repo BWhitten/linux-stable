79cd9c16911e78558501a75485ca9c2fb7cd3e52
Andreas Färber <afaerber@suse.de>
2018-06-27 00:18:36 +0200

    net: Reserve protocol numbers for LoRa
    LoRa is a long-range, low-power wireless network technology by Semtech.
    It serves as base for LoRaWAN as well as multiple proprietary protocols.
    
    AF_LORA
    PF_LORA
    ARPHRD_LORA
    ETH_P_LORA
    
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- security/selinux/include/classmap.h
+++ security/selinux/include/classmap.h
@@ -244,11 +244,13 @@ struct security_class_mapping secclass_map[] = {
 	  {"map_create", "map_read", "map_write", "prog_load", "prog_run"} },
 	{ "xdp_socket",
 	  { COMMON_SOCK_PERMS, NULL } },
+	{ "lora_socket",
+	  { COMMON_SOCK_PERMS, NULL } },
 	{ "perf_event",
 	  {"open", "cpu", "kernel", "tracepoint", "read", "write"} },
 	{ NULL }
   };
 
-#if PF_MAX > 45
+#if PF_MAX > 47
 #error New address family defined, please update secclass_map.
 #endif
