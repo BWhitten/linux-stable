716b31d12be5b63ccc80c2b27b93fa4d3784fef2
Ben Whitten <ben.whitten@gmail.com>
2018-08-07 07:45:09 +0100

    net: lora: sx1257: add regmap and fields and read version

--- drivers/net/lora/sx1257.c
+++ drivers/net/lora/sx1257.c
@@ -3,6 +3,7 @@
  * Semtech SX1255/SX1257 LoRa transceiver
  *
  * Copyright (c) 2018 Andreas Färber
+ * Copyright (c) 2018 Ben Whitten
  *
  * Based on SX1301 HAL code:
  * Copyright (c) 2013 Semtech-Cycleo
@@ -11,12 +12,116 @@
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/of_device.h>
+#include <linux/regmap.h>
 #include <linux/spi/spi.h>
+
+#include "sx125x.h"
 
 #define REG_CLK_SELECT		0x10
 
 #define REG_CLK_SELECT_TX_DAC_CLK_SELECT_CLK_IN	BIT(0)
 #define REG_CLK_SELECT_CLK_OUT			BIT(1)
+
+enum sx125x_fields {
+	F_PA_DRIVER_EN,
+	F_TX_EN,
+	F_RX_EN,
+	F_STDBY_EN,
+	F_TX_DAC_GAIN,
+	F_TX_MIXER_GAIN,
+	F_TX_PLL_BW,
+	F_TX_ANA_BW,
+	F_TX_DAC_BW,
+	F_RX_LNA_GAIN,
+	F_RX_BB_GAIN,
+	F_RX_LNA_Z_IN,
+	F_RX_PLL_BW,
+	F_RX_ADC_BW,
+	F_RX_ADC_TRIM,
+	F_RX_BB_BW,
+	F_RX_ADC_TEMP,
+	F_DIO0_MAP,
+	F_DIO1_MAP,
+	F_DIO2_MAP,
+	F_DIO3_MAP,
+	F_DIG_LOOPBACK,
+	F_RF_LOOPBACK,
+	F_CLK_OUT,
+	F_TX_DAC_CLK_SEL,
+	F_LOW_BAT_EN,
+	F_PLL_LOCK_RX,
+	F_PLL_LOCK_TX,
+	F_LOW_BAT_THRES,
+	F_SX1257_XOSC_GM_STARTUP,
+	F_SX1257_XOSC_DISABLE_REG,
+	F_SX1257_XOSC_DISABLE_AMP,
+	F_SX1257_XOSC_DISABLE_CORE,
+};
+
+static const struct reg_field sx125x_reg_fields[] = {
+	/* MODE */
+	[F_PA_DRIVER_EN]   = REG_FIELD(SX125X_MODE, 3, 3),
+	[F_TX_EN]          = REG_FIELD(SX125X_MODE, 2, 2),
+	[F_RX_EN]          = REG_FIELD(SX125X_MODE, 1, 1),
+	[F_STDBY_EN]       = REG_FIELD(SX125X_MODE, 0, 0),
+	/* TX_GAIN */
+	[F_TX_DAC_GAIN]    = REG_FIELD(SX125X_TX_GAIN, 4, 6),
+	[F_TX_MIXER_GAIN]  = REG_FIELD(SX125X_TX_GAIN, 0, 3),
+	/* TX_BW */
+	[F_TX_PLL_BW]      = REG_FIELD(SX125X_TX_BW, 5, 6),
+	[F_TX_ANA_BW]      = REG_FIELD(SX125X_TX_BW, 0, 4),
+	/* TX_DAC_BW */
+	[F_TX_DAC_BW]      = REG_FIELD(SX125X_TX_DAC_BW, 0, 2),
+	/* RX_ANA_GAIN */
+	[F_RX_LNA_GAIN]    = REG_FIELD(SX125X_RX_ANA_GAIN, 5, 7),
+	[F_RX_BB_GAIN]     = REG_FIELD(SX125X_RX_ANA_GAIN, 1, 4),
+	[F_RX_LNA_Z_IN]    = REG_FIELD(SX125X_RX_ANA_GAIN, 0, 0),
+	/* RX_BW */
+	[F_RX_ADC_BW]      = REG_FIELD(SX125X_RX_BW, 5, 7),
+	[F_RX_ADC_TRIM]    = REG_FIELD(SX125X_RX_BW, 2, 4),
+	[F_RX_BB_BW]       = REG_FIELD(SX125X_RX_BW, 0, 1),
+	/* RX_PLL_BW */
+	[F_RX_PLL_BW]      = REG_FIELD(SX125X_RX_PLL_BW, 1, 2),
+	[F_RX_ADC_TEMP]    = REG_FIELD(SX125X_RX_PLL_BW, 0, 0),
+	/* DIO_MAPPING */
+	[F_DIO0_MAP]       = REG_FIELD(SX125X_DIO_MAPPING, 6, 7),
+	[F_DIO1_MAP]       = REG_FIELD(SX125X_DIO_MAPPING, 4, 5),
+	[F_DIO2_MAP]       = REG_FIELD(SX125X_DIO_MAPPING, 2, 3),
+	[F_DIO3_MAP]       = REG_FIELD(SX125X_DIO_MAPPING, 0, 1),
+	/* CLK_SELECT */
+	[F_DIG_LOOPBACK]   = REG_FIELD(SX125X_CLK_SELECT, 3, 3),
+	[F_RF_LOOPBACK]    = REG_FIELD(SX125X_CLK_SELECT, 2, 2),
+	[F_CLK_OUT]        = REG_FIELD(SX125X_CLK_SELECT, 1, 1),
+	[F_TX_DAC_CLK_SEL] = REG_FIELD(SX125X_CLK_SELECT, 0, 0),
+	/* MODE_STATUS */
+	[F_LOW_BAT_EN]     = REG_FIELD(SX125X_MODE_STATUS, 2, 2),
+	[F_PLL_LOCK_RX]    = REG_FIELD(SX125X_MODE_STATUS, 1, 1),
+	[F_PLL_LOCK_TX]    = REG_FIELD(SX125X_MODE_STATUS, 0, 0),
+	/* LOW_BAT_THRES */
+	[F_LOW_BAT_THRES]  = REG_FIELD(SX125X_LOW_BAT_THRES, 0, 2),
+	/* XOSC */ /* TODO maybe make this dynamic */
+	[F_SX1257_XOSC_GM_STARTUP]  = REG_FIELD(SX1257_XOSC, 0, 3),
+	[F_SX1257_XOSC_DISABLE_REG]  = REG_FIELD(SX1257_XOSC, 4, 4),
+	[F_SX1257_XOSC_DISABLE_CORE]  = REG_FIELD(SX1257_XOSC, 5, 5),
+	[F_SX1257_XOSC_DISABLE_AMP]  = REG_FIELD(SX1257_XOSC, 6, 6),
+};
+
+static struct regmap_config sx125x_spi_regmap_config = {
+        .reg_bits = 8,
+        .val_bits = 8,
+
+        .cache_type = REGCACHE_NONE,
+
+        .read_flag_mask = 0,
+        .write_flag_mask = BIT(7),
+
+        .max_register = SX125X_MAX_REGISTER,
+};
+
+struct sx125x_priv {
+	struct regmap		*regmap;
+	struct regmap_field	*regmap_fields[ARRAY_SIZE(sx125x_reg_fields)];
+};
 
 static int sx1257_write(struct spi_device *spi, u8 reg, u8 val)
 {
@@ -35,18 +140,51 @@ static int sx1257_read(struct spi_device *spi, u8 reg, u8 *val)
 
 static int sx1257_probe(struct spi_device *spi)
 {
+	struct sx125x_priv *priv;
 	u8 val;
 	int ret;
+	int i;
+	unsigned int version;
 
-	if (false) {
-		ret = sx1257_read(spi, 0x07, &val);
-		if (ret) {
-			dev_err(&spi->dev, "version read failed\n");
-			return ret;
-		}
+        spi->mode = SPI_MODE_0;
+        spi->bits_per_word = 8;
+        spi->max_speed_hz = 10000000;
+        ret = spi_setup(spi);
+        if (ret) {
+                dev_err(&spi->dev, "SPI setup failed.\n");
+                return ret;
+        }
 
-		dev_info(&spi->dev, "SX125x version: %02x\n", (unsigned)val);
-	}
+	priv = devm_kzalloc(&spi->dev, sizeof(*priv), GFP_KERNEL);
+	if (!priv)
+		return -ENOMEM;
+
+	dev_set_drvdata(&spi->dev, priv);
+
+	priv->regmap = devm_regmap_init_spi(spi, &sx125x_spi_regmap_config);
+        if (IS_ERR(priv->regmap)) {
+                ret = PTR_ERR(priv->regmap);
+                dev_err(&spi->dev, "Regmap allocation failed: %d\n", ret);
+                return ret;
+        }
+
+	for (i = 0; i < ARRAY_SIZE(sx125x_reg_fields); i++) {
+                const struct reg_field *reg_fields = sx125x_reg_fields;
+
+                priv->regmap_fields[i] = devm_regmap_field_alloc(&spi->dev,
+                                priv->regmap,
+                                reg_fields[i]);
+                if (IS_ERR(priv->regmap_fields[i])) {
+                        ret = PTR_ERR(priv->regmap_fields[i]);
+                        dev_err(&spi->dev, "Cannot allocate regmap field: %d\n", ret);
+                        return ret;
+                }
+        }
+
+	ret = regmap_read(priv->regmap, SX125X_VERSION, &version);
+	if (ret)
+		return ret;
+        dev_info(&spi->dev, "SX125x version: %d\n", version);
 
 	val = REG_CLK_SELECT_TX_DAC_CLK_SELECT_CLK_IN;
 	if (strcmp(spi->controller->dev.of_node->name, "radio-b") == 0) { /* HACK */
@@ -104,4 +242,5 @@ module_spi_driver(sx1257_spi_driver);
 
 MODULE_DESCRIPTION("SX1257 SPI driver");
 MODULE_AUTHOR("Andreas Färber <afaerber@suse.de>");
+MODULE_AUTHOR("Ben Whitten <ben.whitten@gmail.com>");
 MODULE_LICENSE("GPL");
---
+++ drivers/net/lora/sx125x.h
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Semtech SX1255/SX1257 LoRa transceiver
+ *
+ * Copyright (c) 2018   Ben Whitten
+ */
+
+#ifndef _SX125X_
+#define _SX125X_
+
+#define SX125X_MODE		0x00
+#define SX125X_FRF_RX_MSB	0x01
+#define SX125X_FRF_RX_MID	0x02
+#define SX125X_FRF_RX_LSB	0x03
+#define SX125X_FRF_TX_MSB	0x04
+#define SX125X_FRF_TX_MID	0x05
+#define SX125X_FRF_TX_LSB	0x06
+#define SX125X_VERSION		0x07
+#define SX125X_TX_GAIN		0x08
+#define SX125X_TX_BW		0x0A
+#define SX125X_TX_DAC_BW	0x0B
+#define SX125X_RX_ANA_GAIN	0x0C
+#define SX125X_RX_BW		0x0D
+#define SX125X_RX_PLL_BW	0x0E
+#define SX125X_DIO_MAPPING	0x0F
+#define SX125X_CLK_SELECT	0x10
+#define SX125X_MODE_STATUS	0x11
+#define SX125X_LOW_BAT_THRES	0x1A
+
+#define SX1257_XOSC		0x26
+#define SX1255_XOSC		0x28
+
+#define SX125X_MAX_REGISTER	0x1A
+
+#endif
