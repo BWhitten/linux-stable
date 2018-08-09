c21e518ca92b25db4e301dd44ddf33772369b0e6
Ben Whitten <ben.whitten@gmail.com>
2018-08-09 13:33:37 +0100

    net: lora: sx1301: Add initial registration for regmap
    The register and bit-field definitions are taken from the SX1301
    datasheet version 2.01 dated June 2014 with the revision information
    'First released version'.
    
    The reset state and RW capability of each field is not reflected in this
    patch however from the datasheet:
    "Bits and registers that are not documented are reserved. They may
    include calibration values. It is important not to modify these bits and
    registers. If specific bits must be changed in a register with reserved
    bits, the register must be read first, specific bits modified while
    masking reserved bits and then the register can be written."
    
    Then goes on to state:
    "Reserved bits should be written with their reset state, they may be
    read different states."
    
    Caching is currently disabled.
    
    The version is read back using regmap_read to verify regmap operation,
    in doing so needs to be moved after priv and regmap allocation.
    
    Further registers or fields are added as they are required in conversion.
    
    Signed-off-by: Ben Whitten <ben.whitten@lairdtech.com>
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- drivers/net/lora/sx1301.h
+++ drivers/net/lora/sx1301.h
@@ -15,4 +15,14 @@
 #define SX1301_MCU_AGC_FW_VERSION 4
 #define SX1301_MCU_AGC_CAL_FW_VERSION 2
 
+/* Page independent */
+#define SX1301_PAGE     0x00
+#define SX1301_VER      0x01
+
+#define SX1301_VIRT_BASE    0x100
+#define SX1301_PAGE_LEN     0x80
+#define SX1301_PAGE_BASE(n) (SX1301_VIRT_BASE + (SX1301_PAGE_LEN * n))
+
+#define SX1301_MAX_REGISTER         (SX1301_PAGE_BASE(3) + 0x7F)
+
 #endif
