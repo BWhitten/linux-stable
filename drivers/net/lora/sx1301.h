180f04bc60f63abe4cd8bfc4a721e70b7d77a03e
Ben Whitten <ben.whitten@gmail.com>
2018-08-09 13:33:36 +0100

    net: lora: sx1301: Replace version and size magic numbers with defines
    We replace the hard coded numbers for size and version with meaningful
    names.
    
    Signed-off-by: Ben Whitten <ben.whitten@lairdtech.com>
    Signed-off-by: Andreas Färber <afaerber@suse.de>

---
+++ drivers/net/lora/sx1301.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Semtech SX1301 LoRa concentrator
+ *
+ * Copyright (c) 2018   Ben Whitten
+ */
+
+#ifndef _SX1301_
+#define _SX1301_
+
+#define SX1301_CHIP_VERSION 103
+
+#define SX1301_MCU_FW_BYTE 8192
+#define SX1301_MCU_ARB_FW_VERSION 1
+#define SX1301_MCU_AGC_FW_VERSION 4
+#define SX1301_MCU_AGC_CAL_FW_VERSION 2
+
+#endif
