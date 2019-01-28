83871c64ad2a4afa09b2179440da0e7869a23780
Ben Whitten <ben.whitten@lairdtech.com>
2019-01-28 11:47:16 +0000

    net: lora: introduce lora socket addressing for metadata
    Information such as spreading factor, coding rate and power are on a per
    transmission basis so we can encode this information in the lora socket
    address.
    In future we may have a different format for receive with additional
    fields which get populated.
    
    Signed-off-by: Ben Whitten <ben.whitten@lairdtech.com>

--- include/uapi/linux/lora.h
+++ include/uapi/linux/lora.h
@@ -10,6 +10,18 @@
 #include <linux/types.h>
 #include <linux/socket.h>
 
+/* TX addressing definition */
+struct tx_addr {
+	__u64 freq;
+	__u8 sf;
+	__u8 cr;
+	__u16 bw;
+
+	__u8 sync;
+
+	__s8 power;
+};
+
 /* particular protocols of the protocol family PF_LORA */
 #define LORA_PROTO_DATAGRAM	0
 #define LORA_NPROTO		1
@@ -17,7 +29,9 @@
 struct sockaddr_lora {
 	__kernel_sa_family_t lora_family;
 	int lora_ifindex;
+	__u8 lora_protocol;
 	union {
+		struct tx_addr	tx;
 	} lora_addr;
 };
 
