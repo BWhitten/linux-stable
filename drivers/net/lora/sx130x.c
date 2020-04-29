2f8c1073f212e6a60d8e299752c42f579c231d9c
Ben Whitten <ben.whitten@gmail.com>
2020-04-29 19:43:27 +0100

    net: lora: sx130x: add filtering for the tx chain
    The HAL sets filtering based on the bw of the outgoing transmission
    
    Signed-off-by: Ben Whitten <ben.whitten@gmail.com>

--- drivers/net/lora/sx130x.c
+++ drivers/net/lora/sx130x.c
@@ -1108,6 +1108,11 @@ static int sx130x_tx(struct sx130x_priv *priv, struct sk_buff *skb)
 
 	/* TODO 2 Msb in tx_freq0 for large narrow filtering, unset for now */
 	hdr->tx_freq[0] &= 0x3F;
+	if (cfg_bandwidth == 125) {
+		hdr->tx_freq[0] |= 0x40;
+	} else if (cfg_bandwidth == 500) {
+		hdr->tx_freq[0] |= 0x80;
+	}
 
 	/* Copy the TX data into the buffer ready to go */
 
